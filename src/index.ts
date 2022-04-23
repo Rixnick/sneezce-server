8164097a1964eab4da1514e8c44f517e62ecb44b
Rixnick <rixnickl2@gmail.com>
2022-04-23 21:42:09 +0700

    socket io and redis initial commit

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import 'reflect-metadata';
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParse from 'cookie-parser';
+// import socketIO from 'socket.io';
+// import redis from 'redis';
 // import cors from 'cors';
 
 const app = express();
@@ -14,9 +16,15 @@ const frontend_uri = process.env.FRONTEND_URI!;
 //Import apolloServer
 import apolloServer from './apolloServer';
 
+//ToDo: ***************** 1 *********************
+//Config Socket.IO & Redis on node graphql Server
+
+
+// const io = socketIO(apolloServer);
+
 const startServer = async () => {
-   //Databse Connection
-   await mongoose.connect(`${uri}`);
+  //Databse Connection
+  await mongoose.connect(`${uri}`);
 
   
   //App Middleware
@@ -34,11 +42,17 @@ const startServer = async () => {
         origin: frontend_uri,
         credentials: true
       } 
-  })
+  });
+
+  //Socket IO Server
+  // io.on('connection', (socket: any) => {
+  //   console.log('A client connected', socket.id)
+  // });
 
   app.listen(port, () => {
     console.log(`🚀Server started: http://localhost:${port}${server.graphqlPath}`)
   })
 }
+
 
 startServer();
